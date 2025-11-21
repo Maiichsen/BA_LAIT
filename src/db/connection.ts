f2b660ff6c3bba476b4964a60a793d866c9cc7b2
Isabella1807 <megaoreomuffin@hotmail.com>
2025-11-21 12:18:16 +0100

    generated table types from supabase

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,6 +1,7 @@
 import {createClient} from '@supabase/supabase-js';
+import type {Database} from '../../database.types.ts';
 
-export const supabase = createClient(
+export const supabase = createClient<Database>(
   import.meta.env.VITE_SUPPABASE_URL,
   import.meta.env.VITE_SUPPABASE_KEY,
 );
