26d095af1b20573c9b91012152fee22589bd046c
Isabella1807 <megaoreomuffin@hotmail.com>
2025-12-09 13:50:39 +0100

    Added default page content on course create

---
+++ src/constants/courseConstants.ts
@@ -0,0 +1,14 @@
+
+export const defaultPageContent = {
+  backgroundColor: 'ADD8E6',
+  content: [
+    {
+      type: 'h1',
+      text: 'Ny side',
+    },
+    {
+      type: 'p',
+      text: 'Indhold',
+    },
+  ],
+};
