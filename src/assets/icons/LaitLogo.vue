99305f1355f88fe4b5fc7a9b60ae44b8a51583d3
Mai Jockwich <mmjockwich@gmail.com>
2025-12-10 15:59:01 +0100

    Add footer with logo and links
    Introduces a footer component displaying the logo, navigation links, and contact information.
    
    This provides a consistent footer across the site and enhances user experience by providing key links and contact details.

---
+++ src/assets/icons/LaitLogo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+	class?: string;
+}>();
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="237" height="87" viewBox="0 0 237 87" fill="none">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M0 0L0 86.8541L52.1066 86.8541V79.8577L8.61999 79.8577L8.61999 0L0 0Z" fill="#FAFAFA"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M159.15 86.8541H167.77L167.77 0L159.15 0L159.15 86.8541Z" fill="#FAFAFA"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M184.606 0.145996V7.1434L228.093 7.1434L228.093 87.0001H236.714L236.714 0.145996L184.606 0.145996Z" fill="#FAFAFA"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M98.5274 0L62.6523 86.8541H71.5261L82.3003 59.5909L113.352 59.5909L110.781 52.5946L85.2168 52.5946L103.091 6.99741L134.401 86.8541H143.276L108.289 0L98.5274 0Z" fill="#FAFAFA"/>
+  </svg>
+</template>
