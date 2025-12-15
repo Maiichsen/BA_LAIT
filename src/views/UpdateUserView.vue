3b7534fe8c97a32a8a9032fe22e4af8460822515
Isabella1807 <megaoreomuffin@hotmail.com>
2025-12-15 17:39:12 +0100

    wip

--- src/views/UpdateUserView.vue
+++ src/views/UpdateUserView.vue
@@ -153,32 +153,6 @@ test();*/
 		<br/>
 		<button type="submit" class="hover:text-red-700">log ind</button>
 	</form>
-
-<!--	<form @submit.prevent="handleUpdateUser">
-		<BaseInput
-			input-type="text"
-			placeholder="password"
-			input-id="password"
-			label-text="New password"
-			layout="stacked"
-			v-model="userPassword" />
-		<BaseInput
-			input-type="text"
-			placeholder="firstname"
-			input-id="firstname"
-			label-text="new first name"
-			layout="stacked"
-			v-model="userFirstName" />
-		<BaseInput
-			input-type="text"
-			placeholder="lastname"
-			input-id="lastname"
-			label-text="new last name"
-			layout="stacked"
-			v-model="userLastName" />
-		<br />
-		<button type="submit">Opret bruger</button>
-	</form>-->
 </template>
 
 <style scoped></style>
